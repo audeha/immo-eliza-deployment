58f152c97323c5812d1ed87a36b8bde31e60a452
audrey dehalleux <128788531+audeha@users.noreply.github.com>
2024-02-26 14:27:17 +0100

    update main

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 from fastapi import FastAPI
 
-'''from pydantic import BaseModel
-from predict import predict
-
-import sys
-sys.path.append('../immo-eliza-ml')
-
-class User_input(BaseModel):
-    operat'''
-
 app = FastAPI()
 
 @app.get('/')
