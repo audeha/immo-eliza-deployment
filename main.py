042610088a23bcfe57595e3c1620fc415c5c395e
Mark Shevchenko <markshevchenkopu@Marks-MacBook-Pro-2.local>
2024-02-26 17:00:41 +0100

    update

--- main.py
+++ main.py
@@ -2,13 +2,32 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import csv
 from predict import predict
-
+from typing import Union
 app = FastAPI()
 
 class Item(BaseModel):
     nbr_frontages: int
+    nbr_bedrooms: int
+    latitude: float
+    longitude: float
+    total_area_sqm: float
+    surface_land_sqm: Union[float, None]
+    terrace_sqm: float
+    garden_sqm: float
+    province: str = None
+    heating_type: str = None
+    state_building: str = None
+    property_type: str = None
+    epc: str = None
+    locality: str = None
+    subproperty_type: str = None
+    region: str = None
     fl_terrace: bool
-    equipped_kitchen: str
+    fl_garden: bool
+    fl_swimming_pool: bool
+
+
+    
 
 @app.get("/")
 async def root():
