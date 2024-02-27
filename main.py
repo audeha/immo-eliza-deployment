ad25628f7938aedf60cfac428091006a71f73bbc
Mark Shevchenko <markshevchenkopu@Marks-MacBook-Pro-2.local>
2024-02-27 12:08:25 +0100

    simple model

--- main.py
+++ main.py
@@ -7,31 +7,32 @@ app = FastAPI()
 
 class Item(BaseModel):
     nbr_frontages: int
-    nbr_bedrooms: int
-    latitude: float
-    longitude: float
-    total_area_sqm: float
-    surface_land_sqm: Union[float, None]
-    terrace_sqm: float
-    garden_sqm: float
-    province: str = None
-    heating_type: str = None
-    state_building: str = None
-    property_type: str = None
-    epc: str = None
-    locality: str = None
-    subproperty_type: str = None
-    region: str = None
+    equipped_kitchen: str
+    # nbr_bedrooms: int
+    # latitude: float
+    # longitude: float
+    # total_area_sqm: float
+    # surface_land_sqm: Union[float, None]
+    # terrace_sqm: float
+    # garden_sqm: float
+    # province: str = None
+    # heating_type: str = None
+    # state_building: str = None
+    # property_type: str = None
+    # epc: str = None
+    # locality: str = None
+    # subproperty_type: str = None
+    # region: str = None
     fl_terrace: bool
-    fl_garden: bool
-    fl_swimming_pool: bool
+    # fl_garden: bool
+    # fl_swimming_pool: bool
 
 
     
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "alive"}
 
 @app.post("/predict")
 async def create_item(item: Item):
@@ -39,5 +40,10 @@ async def create_item(item: Item):
         writer = csv.writer(file)
         writer.writerow(item.model_dump().keys())
         writer.writerow(item.model_dump().values())
-    
+
     return predict("csv_file.csv")
+    
+
+
+# run to set up connection:
+# uvicorn main:app --reload
