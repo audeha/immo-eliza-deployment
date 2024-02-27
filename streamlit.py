b4a831939d5ce809f88e96cf662f667e32818672
Audrey <128788531+audeha@users.noreply.github.com>
2024-02-27 14:23:13 +0100

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 
 #Define the URL of the FastAPI endpoint
-FASTAPI_URL = ' https://immo-eliza-deployment.onrender.com/predict'  # Update with your FastAPI endpoint URL
+FASTAPI_URL = 'https://immo-eliza-deployment.onrender.com/predict'  # Update with your FastAPI endpoint URL
 
 #Streamlit App Title
 st.title('Price Prediction Web App')
@@ -12,23 +12,19 @@ st.title('Price Prediction Web App')
 
 #Input features for price prediction
 st.header('Enter Features for Prediction')
-property_type=st.radio("pick property type",['House','appartement'])
-region=st.radio("Pick region",["Flanders","Wallonia","Brussels-Capital"])
-province = st.text_input('Province')
-nbr_bedrooms = st.number_input('Number of Bedrooms:', min_value=0, max_value=10, value=1)
-surface_area = st.number_input('Surface Area (sqm):', min_value=0.0, step=100.0)
-locality = st.text_input('Locality:', 'Enter the locality name')
+equipped_kitchen=st.radio("pick kitchen type",['USA_UNINSTALLED','USA_SEMI_EQUIPPED',
+                                               'USA_INSTALLED', 'NOT_INSTALLED', 'USA_HYPER_EQUIPPED',
+                                               'SEMI_EQUIPPED', 'HYPER_EQUIPPED', 'INSTALLED', 'MISSING'])
+nbr_frontages = st.number_input('Number of Frontages:', min_value=0, max_value=10, value=1)
+fl_terrace = st.number_input('Surface terrace (sqm):', min_value=0.0, step=100.0)
 
 #Button to trigger prediction
 if st.button('Predict Price'):
     # Prepare input data as JSON
     input_data = {
-        'property_type':property_type,
-        'region':region,
-        'province':province,
-        'num_bedrooms': nbr_bedrooms,
-        'surface_area': surface_area,
-        'locality': locality,
+        'equipped_kitchen':equipped_kitchen,
+        'nbr_frontages': nbr_frontages,
+        'fl_terrace': fl_terrace,
         # Add more features as needed
     }
 
