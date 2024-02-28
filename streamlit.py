4adcced659a67f0ad16c9875711a5a50e5457c81
HazemEl <156511160+HazemEldabaa@users.noreply.github.com>
2024-02-28 14:22:40 +0100

    updated streamlit and requirements

--- streamlit.py
+++ streamlit.py
@@ -1,64 +1,201 @@
 import streamlit as st
 import requests
-
+import folium 
+from streamlit_folium import folium_static, st_folium
+from folium.plugins import Draw
+from folium import plugins
 #Define the URL of the FastAPI endpoint
 FASTAPI_URL = 'https://immo-eliza-deployment.onrender.com/predict'  # Update with your FastAPI endpoint URL
+
+loc_coordinates = {
+    "Brussels": (50.8503, 4.3517),
+    "Antwerp": (51.2194, 4.4025),
+    "Liège": (50.8503, 5.6889),
+    "Brugge": (51.2094, 3.2247),
+    "Halle-Vilvoorde": (50.8333, 4.3),
+    "Gent": (51.0543, 3.7174),
+    "Turnhout": (51.3223, 4.9483),
+    "Leuven": (50.8792, 4.7009),
+    "Nivelles": (50.5974, 4.3279),
+    "Oostend": (51.2093, 2.9296),
+    "Aalst": (50.9364, 4.0355),
+    "Charleroi": (50.4101, 4.4447),
+    "Kortrijk": (50.8284, 3.2653),
+    "Hasselt": (50.9311, 5.3375),
+    "Namur": (50.4669, 4.8675),
+    "Mechelen": (51.0259, 4.4773),
+    "Sint-Niklaas": (51.1585, 4.1437),
+    "Mons": (50.4541, 3.9561),
+    "Veurne": (51.0749, 2.6564),
+    "Dendermonde": (51.0259, 4.1059),
+    "Verviers": (50.5891, 5.8667),
+    "Tournai": (50.6052, 3.3879),
+    "Oudenaarde": (50.8466, 3.611),
+    "Soignies": (50.5796, 4.0714),
+    "Thuin": (50.3397, 4.2859),
+    "Mouscron": (50.7399, 3.2069),
+    "Dinant": (50.2606, 4.9125),
+    "Tongeren": (50.7805, 5.4645),
+    "Maaseik": (51.0984, 5.7886),
+    "Ath": (50.6303, 3.7806),
+    "Huy": (50.5201, 5.2394),
+    "Marche-en-Famenne": (50.2232, 5.3485),
+    "Waremme": (50.6986, 5.256),
+    "Neufchâteau": (49.8412, 5.4429),
+    "Arlon": (49.6837, 5.8149),
+    "Diksmuide": (51.0339, 2.8614),
+    "Virton": (49.5665, 5.5232),
+    "Bastogne": (50.0039, 5.7215),
+    "Philippeville": (50.1669, 4.5475),
+    "Roeselare": (50.9489, 3.121),
+    "Eeklo": (51.1871, 3.5492),
+    "Tielt": (50.999, 3.3396),
+    "Ieper": (50.8503, 2.8833),
+    "MISSING": (50.8503, 4.3517)  
+}
 
 #Streamlit App Title
 st.title('Price Prediction Web App')
 
 #Image
-#st.image('streamlit', caption='Streamlit Logo', use_column_width=True)
+st.image('streamlit', caption='Streamlit Logo', use_column_width=True)
 
 #Input features for price prediction
-st.header('Enter Features for Prediction')
-equipped_kitchen=st.selectbox("Pick kitchen type",['HYPER_EQUIPPED', 'INSTALLED', 'MISSING', 
-                                                   'NOT_INSTALLED', 'SEMI_EQUIPPED', 'USA_HYPER_EQUIPPED', 
-                                                   'USA_INSTALLED', 'USA_SEMI_EQUIPPED', 'USA_UNINSTALLED']
-)
-nbr_frontages = st.number_input('Number of Frontages:', min_value=0, max_value=10, value=1)
-fl_terrace = st.selectbox('Terrace ?:',  [0, 1])
-fl_garden = st.selectbox('Garden ?:',  [0, 1])
-fl_swimming_pool = st.selectbox('Swimming pool ?:',  [0, 1])
-property_type=st.selectbox("Pick property type",['Apartment','House'])
-longitude = st.slider("Longitude", min_value=5.479947, max_value=6.385048, step=0.000001)
-latitude = st.slider("Latitude", min_value=48.824985, max_value=50.351432, step=0.000001)
+st.header('Please Enter House Specifications for Prediction')
 
-region=st.selectbox("Pick region",['Brussels-Capital', 'Flanders', 'Wallonia'])
-province = st.selectbox('Province', ['Antwerp', 'Brussels', 'East Flanders', 
-                                     'Flemish Brabant', 'Hainaut', 'Liège', 'Limburg', 'Luxembourg', 'MISSING', 
-                                     'Namur', 'Walloon Brabant', 'West Flanders']
-)
-heating_type = st.selectbox('Type of heating:', ['CARBON', 'ELECTRIC', 'FUELOIL', 'GAS', 
-                                                 'MISSING', 'PELLET', 'SOLAR', 'WOOD']
-)
-state_building = st.selectbox('State of building:', ['AS_NEW', 'GOOD', 'JUST_RENOVATED', 
-                                                     'MISSING', 'TO_BE_DONE_UP', 'TO_RENOVATE', 
-                                                     'TO_RESTORE']
-)
-
-epc = st.selectbox('EPC:', ['A', 'A+', 'A++', 'B', 
-                            'C', 'D', 'E', 'F', 'G', 'MISSING']
-)
-subproperty_type = st.selectbox('Select type of subproperty:',['APARTMENT', 'APARTMENT_BLOCK', 'BUNGALOW', 'CASTLE', 
-                                                               'CHALET', 'COUNTRY_COTTAGE', 'DUPLEX', 'EXCEPTIONAL_PROPERTY', 
-                                                               'FARMHOUSE', 'FLAT_STUDIO', 'GROUND_FLOOR', 'HOUSE', 'KOT', 'LOFT', 
-                                                               'MANSION', 'MANOR_HOUSE', 'MIXED_USE_BUILDING', 'OTHER_PROPERTY', 
-                                                               'PENTHOUSE', 'SERVICE_FLAT', 'TOWN_HOUSE', 'TRIPLEX', 'VILLA']
-)
 nbr_bedrooms = st.number_input('Number of Bedrooms:', min_value=0, max_value=10, value=1)
-surface_area = st.number_input('Surface Area (sqm):', min_value=0.0, step=10.0)
-garden_sqm = st.number_input('Garden Area (sqm):', min_value=0.0, step=10.0)
+nbr_frontages = st.number_input('Number of Frontages:', min_value=0, max_value=10, value=1)
 total_area_sqm = st.number_input('Living Area (sqm):', min_value=0.0, step=10.0)
 surface_land_sqm = st.number_input('Plot Area (sqm):', min_value=0.0, step=10.0)
+fl_terrace = st.selectbox('Terrace ?:',  [0, 1])
 terrace_sqm = st.number_input('Terrace Area (sqm):', min_value=0.0, step=2.0)
+fl_garden = st.selectbox('Garden ?:',  [0, 1])
+garden_sqm = st.number_input('Garden Area (sqm):', min_value=0.0, step=10.0)
+property_type=st.selectbox("Pick property type",['House','appartement'])
+subproperty_type = st.selectbox('Select type of subproperty:',[
+    "HOUSE",
+    "APARTMENT",
+    "VILLA",
+    "GROUND_FLOOR",
+    "APARTMENT_BLOCK",
+    "MIXED_USE_BUILDING",
+    "PENTHOUSE",
+    "DUPLEX",
+    "FLAT_STUDIO",
+    "EXCEPTIONAL_PROPERTY",
+    "TOWN_HOUSE",
+    "SERVICE_FLAT",
+    "MANSION",
+    "BUNGALOW",
+    "KOT",
+    "LOFT",
+    "FARMHOUSE",
+    "COUNTRY_COTTAGE",
+    "MANOR_HOUSE",
+    "TRIPLEX",
+    "OTHER_PROPERTY",
+    "CHALET",
+    "CASTLE"
+])
+
+state_building = st.selectbox('State of building:', [
+    "MISSING",
+    "GOOD",
+    "AS_NEW",
+    "TO_RENOVATE",
+    "TO_BE_DONE_UP",
+    "JUST_RENOVATED",
+    "TO_RESTORE"
+])
+
+epc = st.selectbox('EPC:', [
+    "MISSING",
+    "B",
+    "C",
+    "D",
+    "A",
+    "F",
+    "E",
+    "G",
+    "A+",
+    "A++"
+])
+heating_type = st.selectbox('Type of heating:', [
+    "GAS",
+    "MISSING",
+    "FUELOIL",
+    "ELECTRIC",
+    "PELLET",
+    "WOOD",
+    "SOLAR",
+    "CARBON"
+])
+equipped_kitchen=st.selectbox("Pick kitchen type",['USA_UNINSTALLED','USA_SEMI_EQUIPPED',
+                                               'USA_INSTALLED', 'NOT_INSTALLED', 'USA_HYPER_EQUIPPED',
+                                               'SEMI_EQUIPPED', 'HYPER_EQUIPPED', 'INSTALLED', 'MISSING'])
+fl_swimming_pool = st.selectbox('Swimming pool ?:',  [0, 1])
+# List of regions and their corresponding provinces
+region_province_mapping = {
+    "Flanders": ["West Flanders", "East Flanders", "Antwerp", "Limburg", "Flemish Brabant","MISSING"],
+    "Wallonia": ["Hainaut", "Liège", "Walloon Brabant", "Namur", "Luxembourg","MISSING"],
+    "Brussels-Capital": ["Brussels","MISSING"]
+}
+
+# List of localities for each province
+province_locality_mapping = {
+    "Antwerp": ["Antwerp", "Turnhout", "Mechelen"],
+    "East Flanders": ["Gent", "Aalst", "Sint-Niklaas", "Dendermonde", "Oudenaarde", "Eeklo"],
+    "Brussels": ["Brussels"],
+    "Walloon Brabant": ["Nivelles"],
+    "Flemish Brabant": ["Halle-Vilvoorde", "Leuven"],
+    "Liège": ["Liège", "Verviers", "Huy", "Waremme"],
+    "West Flanders": ["Brugge", "Oostend", "Kortrijk", "Veurne", "Diksmuide", "Roeselare", "Tielt", "Ieper"],
+    "Hainaut": ["Charleroi", "Mons", "Tournai", "Soignies", "Thuin", "Mouscron", "Ath"],
+    "Luxembourg": ["Marche-en-Famenne", "Neufchâteau", "Arlon", "Virton", "Bastogne"],
+    "Limburg": ["Hasselt", "Tongeren", "Maaseik"],
+    "Namur": ["Namur", "Dinant", "Philippeville"],
+    "MISSING": ["MISSING"]
+}
 
 
-locality = st.selectbox('Locality:', ['Aalst', 'Antwerp', 'Arlon', 'Ath', 'Bastogne', 
-                                      'Brugge', 'Brussels', 'Charleroi', 'Dendermonde', 
-                                      'Diksmuide', 'Dinant', 'Eeklo', 'Gent', 'Halle-Vilvoorde', 
-                                      'Hasselt', 'Huy', 'Ieper', 'Kortrijk', 'Leuven', 'Liège', 
-                                      'Maaseik', 'Marche-en'])
+# User selects the region
+region = st.selectbox("Pick region", list(region_province_mapping.keys()))
+
+# Based on the selected region, dynamically update the options for the province
+provinces_for_region = region_province_mapping.get(region, [])
+province = st.selectbox('Province', provinces_for_region)
+
+# Based on the selected province, dynamically update the options for the locality
+localities_for_province = province_locality_mapping.get(province, [])
+locality = st.selectbox('Locality:', localities_for_province)
+
+for p,c in loc_coordinates.items():
+    if p == locality:
+        latitude = c[0]
+        longitude = c[1]        
+  
+# Initial coordinates for Brussels
+st.title("Property Location")
+
+#belgium_coords = [50.8503, 4.3517]  # Latitude and Longitude for Brussels, Belgium
+m = folium.Map(location=[latitude,longitude], zoom_start=12)
+Draw(export=True).add_to(m)
+
+
+# Call to render Folium map in Streamlit
+st_data = st_folium(m, width=725)
+
+if st_data is not None and st_data.get("last_active_drawing") is not None:
+    # Accessing coordinates from the last_active_drawing
+    coordinates = st_data["last_active_drawing"].get("geometry", {}).get("coordinates")
+
+    if coordinates is not None and len(coordinates) == 2:
+        # Extract latitude and longitude
+        latitude, longitude = coordinates
+        st.write(f"Last Clicked Latitude: {latitude}")
+        st.write(f"Last Clicked Longitude: {longitude}")
+else:
+    st.warning("Please select the marker and click on the map to retrieve coordinates")
 
 
 
